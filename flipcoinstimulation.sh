f7663185ab25aa0b76772d88ea53a0c55dd8b8e2
rojajullapalli <rojajules@gmail.com>
2021-07-24 17:55:06 +0530

    loop to show how many times head and tail has won

--- flipcoinstimulation.sh
+++ flipcoinstimulation.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -x
 echo "Welcome to FlipCoin simulation program"
-coin=$(( RANDOM%2 ))
-echo $coin
-if (( $coin==0 ))
-then
-   echo "Head"
-else
-   echo "Tail"
-fi
+countofhead=0
+countoftail=0
+
+for (( i=0; i<20; i++ ))
+do
+   coin=$(( RANDOM%2 ))
+   if [ $coin -eq 0 ]
+   then
+      echo "Coin flips head"
+      countofhead=$(( $countofhead+1 ))
+   else
+      echo "coin flips tail"
+      countoftail=$(( $countoftail+1 ))
+   fi
+done
+echo "Number of times head won" $countofhead
+echo "Number of times tail won" $countoftail
