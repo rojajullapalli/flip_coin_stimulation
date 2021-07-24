def3d2ac51598670beac4ae1855b5a058389e2d1
rojajullapalli <rojajules@gmail.com>
2021-07-24 17:48:08 +0530

    flipping a coin to display head or tail as winner

--- flipcoinstimulation.sh
+++ flipcoinstimulation.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
-echo "welcome to flip coin stimulation"
+echo "Welcome to FlipCoin simulation program"
+coin=$(( RANDOM%2 ))
+echo $coin
+if (( $coin==0 ))
+then
+   echo "Head"
+else
+   echo "Tail"
+fi
